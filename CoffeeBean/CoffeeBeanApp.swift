8b5d3e0611f540191e1a7fa440ff43bd9c14847a
dsv <zxcv>
2024-12-07 00:15:46 +0500

    Initial Commit

---
+++ CoffeeBean/CoffeeBeanApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeBeanApp.swift
+//  CoffeeBean
+//
+//  Created by Muhammad Rizwan on 07/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeBeanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
